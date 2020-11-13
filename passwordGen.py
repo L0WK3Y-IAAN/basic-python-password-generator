64185359cf7de7006fec0b908662dee7972e7eb6
unknown <l0wk3y@iaansec.net>
2020-11-13 14:09:06 -0500

    shortened

--- passwordGen.py
+++ passwordGen.py
@@ -3,10 +3,9 @@ from random import *
 import subprocess
 
 subprocess.call(["cls"],shell=True) #Change "cls" to "clear" for linux
-numInput = input("Enter password length: ")
-numInt = int(numInput)
+numInput = int(input("Enter password length: "))
 
 chars = string.ascii_letters + string.punctuation  + string.digits
 subprocess.call(["cls"],shell=True)
-pword =  ''.join(choice(chars) for x in range(numInt))
+pword =  ''.join(choice(chars) for x in range(numInput))
 print ("Password:", pword)
